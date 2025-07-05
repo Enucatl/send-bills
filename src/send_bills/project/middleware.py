18abf7e8b94350b848e423d33cd042977159dbfc
Matteo Abis <1423701+Enucatl@users.noreply.github.com>
2025-07-05 18:06:27 +0200

    set unusable password

--- src/send_bills/project/middleware.py
+++ src/send_bills/project/middleware.py
@@ -5,8 +5,12 @@ class CustomHeaderRemoteUserMiddleware(PersistentRemoteUserMiddleware):
     header = "HTTP_REMOTE_USER"
 
     def configure_user(self, user):
+        print(user)
+        print(f"{user.is_staff=}")
+        print(f"{user.is_superuser=}")
         if not user.is_staff or not user.is_superuser:
             user.is_staff = True
             user.is_superuser = True
+            user.set_unusable_password()
             user.save()
         return user
