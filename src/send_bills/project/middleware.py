920030381631257a576f8adf78e9bc2efcfbfa4b
Matteo Abis <1423701+Enucatl@users.noreply.github.com>
2025-07-05 17:54:42 +0200

    make remote users superusers to access the admin site

--- src/send_bills/project/middleware.py
+++ src/send_bills/project/middleware.py
@@ -3,3 +3,10 @@ from django.contrib.auth.middleware import PersistentRemoteUserMiddleware
 
 class CustomHeaderRemoteUserMiddleware(PersistentRemoteUserMiddleware):
     header = "HTTP_REMOTE_USER"
+
+    def configure_user(self, user):
+        if not user.is_staff or not user.is_superuser:
+            user.is_staff = True
+            user.is_superuser = True
+            user.save()
+        return user
