e96537009f1f67824157d7f6c59aebbde9a68195
Matteo Abis <1423701+Enucatl@users.noreply.github.com>
2025-10-02 09:38:54 +0200

    increase gunicorn timeout

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 # Apply database migrations
 .venv/bin/python src/send_bills/manage.py migrate --noinput
 # Start Gunicorn, binding to all interfaces on port 8000
-.venv/bin/python -m gunicorn --bind "[::]:8000" send_bills.project.wsgi:application
+.venv/bin/python -m gunicorn --bind "[::]:8000" --timeout 300 send_bills.project.wsgi:application
